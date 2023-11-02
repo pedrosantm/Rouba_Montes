d6aa2918086d236c99aa17fb29fe8563be041f04
Pedro Costa <pedroo.s.costaa@gmail.com>
2023-11-02 16:40:22 -0300

    criação de estruturras novas

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <conio.h>
 #include "oprecoes.h"
 
 
@@ -13,6 +12,8 @@ int main() {
     int num_jogadores = 0;
     int num_baralhos = 0;
 
+    int retorno_empilhamento;
+
     printf("Jogo rouba montes\n");
 
     printf("Digite o numero de jogadores: ");
@@ -22,8 +23,9 @@ int main() {
     printf("Digite a quantidade de baralhos: ");
     scanf("%d", &num_baralhos);
 
-    pilha baralho;
-    jogador *jogadores = (jogador *)malloc(num_jogadores * sizeof(jogador));
+    pilha baralho_principal;//o baralho prinicpal é de estrutura pilha
+    jogador *jogadores = (jogador *)malloc(num_jogadores * sizeof(jogador));//cria um array dinamico com o numero de jogadores selecionado
+    lista *cartas_mesa = (lista *)malloc(sizeof(lista));
 
     // nome dos jogadores
     for (int i = 0; i < num_jogadores; i++) {
@@ -33,11 +35,14 @@ int main() {
     }
 
 
-    for(int i = 0; i < 52; i++) {
+
+    // preenche o baralho com os numeros e os nipes , usando o controlador numero de baralhos para repetir
+    // processo a qauntidade de vezes nescessarias
+    for(int i = 0; i < num_baralhos; i++) {
         for (int numero = 1; numero <= 13; numero++) {
             for (char nipe = 'P'; nipe <= 'E'; nipe++) {
                 carta carta = {numero, nipe};
-                 empilhar(&baralho, carta);
+                 empilhar_baralho(&baralho_principal, carta);//empilha as cartas no baralho principal
             }
         }
 }
--- oprecoes.h
+++ oprecoes.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <time.h>
 
 
 typedef struct Carta {
@@ -13,9 +14,23 @@ typedef struct Pilha {
     int topo;
 }pilha;
 
+typedef struct elemento{//estruct para definição do elemento
+    carta carta;
+    struct elemento* anterior;
+}elemento;
+
+typedef struct pilha_dinamica{//pilha dinamica para representar cada monte de um jogador jogador
+    elemento* topo;
+}pilha_dinamica;
+
+typedef struct{
+    elemento* inicio;
+}lista;
 
 typedef struct jogador{
     char nome[50];
+    pilha_dinamica monte;
+    lista cartas_jogaveis;
     int numero_de_cartas;
 }jogador;
 
@@ -25,36 +40,31 @@ typedef struct jogador{
 
 
 void inicializarPilha(pilha *baralho) {
-    baralho->topo = -1;  // Inicializa a pilha vazia
+    baralho->topo = -1;
+    return; // Inicializa a pilha vazia
 }
 
-void empilhar(pilha *baralho, carta carta) {
-    if (baralho->topo < 52 - 1) {
-        baralho->cartas[++(baralho->topo)] = carta;
-    }
-}
+
 
 void embaralharPilha(pilha *baralho) {
     
     srand(time(NULL));
     
     // Embaralha a pilha 
-    for (int i = baralho->topo; i > 0; i--) {
+    for (int i = baralho->topo - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         carta temp = baralho->cartas[i];
         baralho->cartas[i] = baralho->cartas[j];
         baralho->cartas[j] = temp;
     }
+    return;
 }
 
-void printPilha(pilha *baralho, int max) {
 
-    int aux = baralho->topo;
-
-    for(int i = 0; i < max; i++){
-        printf("|| %d %c||\n", baralho->cartas->numero[aux], baralho->cartas->nipe);
-        aux--;
+void empilhar_baralho(pilha *baralho, carta carta) {
+    if (baralho->topo < 52 - 1) {
+        baralho->cartas[++(baralho->topo)] = carta;
     }
-
     return ;
 }
+
